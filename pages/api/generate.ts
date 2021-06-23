97d2272eced79fc4596c0f25254bb89adf2960b8
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2021-06-23 15:49:05 +1200

    tile generation start

---
+++ pages/api/generate.ts
@@ -0,0 +1,22 @@
+/*
+    Generate 5x5 tile board for spymasters
+    0 - civilian
+    1 - red
+    2 - blue
+    3 - assassin
+*/
+export default function tiles() {
+    // init board with civilians
+    var tiles:number[][] = [ [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0 ,0] ];
+    // assassin
+    let assassin_row = getRandomInt(5);
+    let assassin_col = getRandomInt(5);
+    tiles[assassin_row][assassin_col] = 3;
+    for (let team = 1; team <= 2; team++) {
+        
+    }
+}
+
+function getRandomInt(max : number) {
+    return Math.floor(Math.random() * max);
+}
