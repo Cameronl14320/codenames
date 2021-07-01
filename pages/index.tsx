008a401fcb9b4f4a890fc0ee6e4e23f6544ffc3d
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2021-07-01 17:02:55 +1200

    designing home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Button, TextField, Input } from '@material-ui/core'
+import { Box, Container, Button, TextField, Input } from '@material-ui/core'
 import { generateWords, generateTiles } from '../game/generate'
 import { useState } from 'react'
 
 export default function Home() {
   var roomCode: String = "";
   return (
-    <div>
-      <TextField value = {roomCode}>
-      </TextField>
-      <Button onClick={() => {generateTiles()} }>
-        Tiles
-      </Button>
-      <Button onClick={() => {generateWords()}}>
-        Words
-      </Button>
-    </div>
+    <Box>
+      <Container>
+        Test
+      </Container>
+    </Box>
   )
 }
+
+// <TextField id="roomcode_input" variant="outlined">
+// </TextField>
+// <Button onClick={() => {generateTiles()} }>
+//   Tiles
+// </Button>
+// <Button onClick={() => {generateWords()}}>
+//   Words
+// </Button>
