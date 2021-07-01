a0203811e3ce839bdc5c614cb5eb3fa8a02a540f
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2021-07-01 16:02:40 +1200

    text field

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Button } from '@material-ui/core'
+import { Button, TextField, Input } from '@material-ui/core'
 import { generateWords, generateTiles } from '../game/generate'
+import { useState } from 'react'
 
 export default function Home() {
+  var roomCode: String = "";
   return (
     <div>
+      <TextField value = {roomCode}>
+      </TextField>
       <Button onClick={() => {generateTiles()} }>
         Tiles
       </Button>
