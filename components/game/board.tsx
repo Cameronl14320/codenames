86dd23967d579597637bd77820d1f2dc32675c5d
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2021-07-10 16:32:03 +1200

    creating board for display

--- components/game/board.tsx
+++ components/game/board.tsx
@@ -1,9 +1,11 @@
 import Game from "../../game/Game";
+import Board from "../../game/Board";
 
 export function generateBoard(props: { game: Game}) {
     const game = props.game;
-    // var words: String[][] = Words();
-    // var tiles: Number[][] = Tiles();
+    var board : Board = game.board;
+    var words : String[][] = board.words;
+    var tiles : String[][] = board.tiles;
 
     // Generate the board according to words
     // Generate the spymaster tiles according to tiles
