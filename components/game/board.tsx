7b4558b43417a6347ff03222bbb56af5bb8db279
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2021-07-10 16:35:26 +1200

    helper functions

--- components/game/board.tsx
+++ components/game/board.tsx
@@ -1,7 +1,7 @@
 import Game from "../../game/Game";
 import Board from "../../game/Board";
 
-export function generateBoard(props: { game: Game}) {
+export default function generateBoard(props: { game: Game}) {
     const game = props.game;
     var board : Board = game.board;
     var words : String[][] = board.words;
@@ -10,9 +10,20 @@ export function generateBoard(props: { game: Game}) {
     // Generate the board according to words
     // Generate the spymaster tiles according to tiles
 
+    var display_words = [];
+    var display_tiles = [];
+
     return (
         <div>
             This is the board
         </div>
     );
+}
+
+function displayTiles(props: { words: String[][]}) {
+    const words = props.words;
+}
+
+function displayWords(props: { tiles: String[][]}) {
+    const tiles = props.tiles;
 }
