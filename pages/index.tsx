9c1132042ba4b0e71914914209f7bd6554ed7f79
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2021-07-12 22:12:24 +1200

    attempting to resolve server client mismatch

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,21 @@ import { generateWords, generateTiles } from './api/game/generate'
 import { useState } from 'react'
 import { css, jsx } from '@emotion/react'
 import GameDisplay from '../components/game/game'
+import Game from './api/game/Game'
 import GenerateBoard from '../components/game/board'
 
 
 
 export default function Home() {
   var roomCode: String = "";
-  var game = GameDisplay();
+  const [game, changeGame] = useState(new Game());
+
+  console.log("gamestate: " + game.board.words);
   return (
     <Box css={{
       textAlign: 'center',
     }}>
-      {game}
+      <GenerateBoard game={game}/>
     </Box>
   )
 }
